3ecefad547fe1b5f5566b5b3c1806d5377b827d8
dungtri <dtle@actiris.be>
2020-01-20 09:22:48 +0100

    Migrate OpenNLP to .NET Standard / .NET Core

---
+++ DataFileConverter.Standard/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using SharpEntropy;
+using SharpEntropy.IO;
+
+namespace DataFileConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Environment.CurrentDirectory + "/../../";
+            var gisModelFilePath = currentDirectory + "../Resources/Models/EnglishSD.nbin";
+
+            var outputFilePath = currentDirectory + "Output/" + Path.GetFileNameWithoutExtension(gisModelFilePath) + ".txt";
+
+            ConvertBinaryGisFileToPlainText(gisModelFilePath, outputFilePath);
+
+            Console.WriteLine("Plain text written at {0}", outputFilePath);
+            Console.ReadKey();
+        }
+
+        private static void ConvertBinaryGisFileToPlainText(string inputFilePath, string outputFilePath)
+        {
+            var reader = new BinaryGisModelReader(inputFilePath);
+            var model = new GisModel(reader);
+
+            var writer = new PlainTextGisModelWriter();
+            writer.Persist(model, outputFilePath);
+        }
+    }
+}
